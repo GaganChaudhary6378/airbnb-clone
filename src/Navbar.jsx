032fc8b789458e0380570db58a6f86c0659da456
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-13 18:27:26 +0530

    processing

---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <nav>
+            <img className="logo-el" src="airbnb.png" />
+        </nav>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
