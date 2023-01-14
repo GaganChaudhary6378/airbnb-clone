8b8d3740b00178559e78bb0815bce7b04b4c2196
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-14 21:29:48 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,20 @@ export default function App(){
         return (
         <Card 
         key={item.id} //thats because for ignore the warning that each should have a different key
-        img={item.coverImg} 
-        rating={item.stats.rating} 
-        reviewCount={item.stats.reviewCount} 
-        country={item.location} 
-        title={item.title} 
-        price={item.price} 
-        spots={item.openSpots}
+        {...item}
+        // item={item} 
+        // and we can use spread operator also 
+        // Like previously we did that id=item.id
+        // img=items.coverImg
+        // So spread operator also works in the same way its assign the value to a variable and it doesnt passes the object
+        // Now we are going to pass all these items in the form of an object rather writing each component separately
+        // img={item.coverImg} 
+        // rating={item.stats.rating} 
+        // reviewCount={item.stats.reviewCount} 
+        // country={item.location} 
+        // title={item.title} 
+        // price={item.price} 
+        // spots={item.openSpots}
         />
         )
     })
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 export default function Card(props) {
   let badgeText
-  if(props.spots===0){
+  if(props.openSpots===0){
     badgeText="SOLD OUT"
-  }else if(props.country==="Online"){
+  }else if(props.location==="Online"){
     badgeText="ONLINE"
   }
   return (
     <div className="card">
       {badgeText && <div className="card-badge">{badgeText}</div>}
-      <img className="card-image" src={props.img} />
+      <img className="card-image" src={props.coverImg} />
       <div className="card-stats">
         <img className="card-star" src="Star 1.png" />
         <span>{props.rating}</span>
         <span className="gray">({props.reviewCount}) •</span>
-        <span className="gray">{props.country}</span>
+        <span className="gray">{props.location}</span>
       </div>  
       <p className="card-title">{props.title}</p>
       <p className="card-price"><span className="bold">From ${props.price}</span>/ person </p>
