b2172f1cfbe202e5a294b5636ce119924e7e2f9a
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-14 20:58:03 +0530

    90% done

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ export default function App(){
     const content=data.map(item => {
         return (
         <Card 
+        key={item.id} //thats because for ignore the warning that each should have a different key
         img={item.coverImg} 
         rating={item.stats.rating} 
         reviewCount={item.stats.reviewCount} 
         country={item.location} 
         title={item.title} 
         price={item.price} 
+        spots={item.openSpots}
         />
         )
     })
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 export default function Card(props) {
+  let badgeText
+  if(props.spots===0){
+    badgeText="SOLD OUT"
+  }else if(props.country==="Online"){
+    badgeText="ONLINE"
+  }
   return (
     <div className="card">
+      {badgeText && <div className="card-badge">{badgeText}</div>}
       <img className="card-image" src={props.img} />
       <div className="card-stats">
         <img className="card-star" src="Star 1.png" />
